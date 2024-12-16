e528a2c5eada4e7de1f48d5327995aa5df295d80
Daniëlle Huisman <danielle@huisman.me>
2024-12-16 11:07:25 +0100

    Update to Leptos v0.7

--- book-example/src/app.rs
+++ book-example/src/app.rs
@@ -1,14 +1,17 @@
-use leptos::*;
+use leptos::prelude::*;
 
 #[component]
 pub fn App() -> impl IntoView {
-    let mut views: Vec<View> = vec![];
+    let mut views: Vec<AnyView> = vec![];
 
     if cfg!(feature = "button") {
         use crate::button::Button;
-        views.push(view! {
-            <Button />
-        });
+        views.push(
+            view! {
+                <Button />
+            }
+            .into_any(),
+        );
     }
 
     views.into_view()
--- book-example/src/button.rs
+++ book-example/src/button.rs
@@ -1,8 +1,8 @@
-use leptos::*;
+use leptos::prelude::*;
 
 #[component]
 pub fn Button() -> impl IntoView {
-    let (count, set_count) = create_signal(0);
+    let (count, set_count) = signal(0);
 
     view! {
         <button on:click=move |_| set_count.update(|count| *count += 1 )>
